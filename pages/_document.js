d34321693b86e82685393cac6d2d1bd35348a24f
Septigooooo <20081010177@student.upnjatim.ac.id>
2025-05-07 09:08:33 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Analytics Script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-CTTH2ZEKWE"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-CTTH2ZEKWE');
+              `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
